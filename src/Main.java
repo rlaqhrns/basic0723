077d213ece96cdeb78132d8d7cde812ef6dd5ae5
rlaqhrns93@naver.com <Green_Computer@DESKTOP-912HOCH>
2020-07-24 18:33:01 +0900

    hi

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,19 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-
+		int x, y, z, sum = 0;
 		for (int i = 1; i <= N; i++) {
+			x = i / 100;
+			y = (i / 10) % 10;
+			z = i % 10;
+			sum = i + x + y + z;
+			if (sum == N) {
+				System.out.println(i);
+				return;
+			}
+
 		}
+		System.out.println(0);
 
 	}
 }
